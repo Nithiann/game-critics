2590c42831229cdc82478c705b0123f4df04ba04
Bas Voss <Voss.Bas@outlook.com>
2022-11-14 18:05:54 +0100

    created interface for review, comment and game

--- libs/api-interfaces/src/lib/api-interfaces.ts
+++ libs/api-interfaces/src/lib/api-interfaces.ts
@@ -18,3 +18,32 @@ export interface User {
   role: userRole;
   user_score: number;
 }
+
+export interface Review {
+  _id: string;
+  user_ref: string;
+  description: string;
+  created_at: Date;
+  modified_at: Date|null;
+  game_score: number;
+  review_score: number;
+  comments: [Comment]|null;
+}
+
+export interface Comment {
+  _id: string;
+  user_ref: string;
+  description: string;
+  created_at: Date;
+  modified_at: Date;
+}
+
+export interface Game {
+  _id: string;
+  title: string;
+  description: string;
+  image: string;
+  genre: [string];
+  score: number;
+  reviews: [Review]|null;
+}
