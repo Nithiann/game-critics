139cf8309d639948472257d598ee96db1356d953
Bas Voss <Voss.Bas@outlook.com>
2022-11-14 18:01:49 +0100

    user interface created

--- libs/api-interfaces/src/lib/api-interfaces.ts
+++ libs/api-interfaces/src/lib/api-interfaces.ts
@@ -1,3 +1,20 @@
 export interface Message {
   message: string;
 }
+
+enum userRole {
+  user,
+  reviewer,
+  admin
+}
+
+export interface User {
+  _id: string;
+  email: string;
+  password: string;
+  firstName: string;
+  lastName: string;
+  age: Date;
+  role: userRole;
+  user_score: number;
+}
