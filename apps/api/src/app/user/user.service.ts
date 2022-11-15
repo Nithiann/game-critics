cee61852b0263e5d3dea13e58600a4c6866febfc
Bas Voss <Voss.Bas@outlook.com>
2022-11-15 14:33:00 +0100

    getAll and getOne functions for user

--- apps/api/src/app/user/user.service.ts
+++ apps/api/src/app/user/user.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User as UserModel, UserDocument } from './user.schema';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectModel(UserModel.name) private userModel: Model<UserDocument>
+  ) {}
+
+  async getAll(): Promise<UserModel[]> {
+    return this.userModel.find()
+  }
+
+  async getOne(id): Promise<UserModel> {
+    return this.userModel.findOne({_id: id})
+  }
+}
