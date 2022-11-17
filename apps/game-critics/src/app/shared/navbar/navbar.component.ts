8aab06c3059e11df079969996fbbcb6fa154c332
Bas Voss <Voss.Bas@outlook.com>
2022-11-17 14:39:41 +0100

    added basic menu layout

--- apps/game-critics/src/app/shared/navbar/navbar.component.ts
+++ apps/game-critics/src/app/shared/navbar/navbar.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {MenubarModule} from 'primeng/menubar';
-import {MenuItem} from 'primeng/api';
+import {MenuItem, PrimeIcons} from 'primeng/api';
 
 
 @Component({
@@ -9,7 +9,49 @@ import {MenuItem} from 'primeng/api';
   styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  menuItems: MenuItem[] = [];
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.menuItems = [
+      {
+        label: 'Home',
+        icon: 'pi pi-fw pi-home',
+        routerLink: '/'
+      },
+      {
+        label: 'Users',
+        icon: 'pi pi-fw pi-users',
+        items: [
+          {
+            label: 'New',
+            icon: 'pi pi-fw pi-user-plus'
+          },
+          {
+            label: 'Overview',
+            icon: 'pi pi-fw pi-table'
+          }
+        ]
+      },
+      {
+        label: 'Games',
+        items: [
+          {
+            label: 'New',
+            icon: 'pi pi-fw pi-plus'
+          },
+          {
+            label: 'Overview',
+            icon: 'pi pi-fw pi-table'
+          }
+        ]
+      },
+      {
+        label: 'About',
+        icon: 'pi pi-fw pi-info',
+        routerLink: '/about'
+      }
+    ]
+  }
 }
