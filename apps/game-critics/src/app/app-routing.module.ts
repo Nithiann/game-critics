ee51af76140e351c6a368ad64b41397bdcdc9d4a
Bas Voss <Voss.Bas@outlook.com>
2022-11-20 22:18:55 +0100

    added new routes

--- apps/game-critics/src/app/app-routing.module.ts
+++ apps/game-critics/src/app/app-routing.module.ts
@@ -4,8 +4,7 @@ import { AboutComponent } from './pages/about/about.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'dashboard' },
-  { path: 'dashboard', pathMatch: 'full', component: DashboardComponent },
+  { path: '', pathMatch: 'full', component: DashboardComponent},
   { path: 'about', pathMatch: 'full', component: AboutComponent },
   {
     path: 'users',
@@ -29,6 +28,10 @@ const routes: Routes = [
       import('./auth/auth.module')
       .then((m) => m.AuthModule),
   },
+  {
+    path: '**',
+    component: DashboardComponent
+  }
 ];
 
 @NgModule({
